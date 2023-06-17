059b00fc43d5f2d2034bd9545e73c1e1a391b525
ierkig <136831251+ierkig@users.noreply.github.com>
2023-06-17 15:28:42 +0900

    Delete src directory

--- src/Item.h
+++
@@ -1,9 +0,0 @@
-class Item{
-    public:
-        int x;
-        int y;
-        bool itemType;
-        int lifeTime;
-        Item(int x, int y, bool itemType, int lifeTime = 5) : x(x), y(y), itemType(itemType), lifeTime(lifeTime) {};
-
-};
